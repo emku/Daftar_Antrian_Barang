542a205f75ad0cf42916665d06515e177d56d882
Mohammad Kukuh <Emku@users-macbook-pro.local>
2016-07-19 14:33:20 +0700

    Initial Commit

---
+++ Daftar_Antrian_Barang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Daftar_Antrian_Barang
+//
+//  Created by Mohammad Kukuh on 7/19/16.
+//  Copyright © 2016 Mohammad Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
